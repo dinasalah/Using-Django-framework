fba438cc3a22c845f4ebd61653b0af66b6f52682
dinasalah <dina.salah@anzmasoft.com>
2021-07-31 15:33:32 +0200

    Update views.py

--- views.py
+++ views.py
@@ -2,28 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 import json
-from itertools import islice
 from datetime import datetime, timedelta
 
 now = datetime.now() - timedelta(10)
 mydate = now.strftime("%Y-%m-%dT%H:%M:%SZ") 
 
 url = 'https://api.github.com/search/repositories?q=created:>' + mydate + '&sort=stars&order=desc&page=1&per_page=3'
-a = requests.get(url)
-#print(a.status_code)
-b = a.json()
-c = b["items"]
+resp = requests.get(url)
+parsedResp = resp.json()
+items = parsedResp["items"]
 
-for x in c:
+for item in items:
 	print("********************************************")
-	print(x['language'])
+	print(item['language'])
 	try:
-		u = 'https://api.github.com/search/repositories?q=language:'+ x['language'] +'&order=desc'
-		#print(u)
-		l = requests.get(u)
-		m = l.json()
+		langUrl = 'https://api.github.com/search/repositories?q=language:'+ item['language'] +'&order=desc'
+		langResp= requests.get(langUrl)
+		langParsedResp = langResp.json()
 		print("Total Repositories count: ")
-		print(m["total_count"])
+		print(langParsedResp["total_count"])
 
 	except:
 		print("Error in dealing with this language")
